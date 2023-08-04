9673ba7c8369a1c925f23106d3b066fa6165a561
nouraalii <119954338+nouraalii@users.noreply.github.com>
2023-08-05 00:20:06 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// For Task 1 
+
+// 1 - Inctroduce Your Self (Name , age  , faculty , ) in varriable and write it in HTML Using JS Note using temolate Literal
+
+        //Write Code Here
+        // Note : you can write in HTML using  document.write(varribale_Name) ; 
+
+
+
+// --> solve problem 1
+let intro=`<div class="intro">
+<h3>Name:Noura</h3>
+<p>Age:20 <br> <br> Faculty:Computer Science</p>`;
+document.write(intro); 
+// end problem 1
+
+
+
+
+
+// 2 - You Have Number 
+
+let str = "20000.432423" ; 
+
+console.log("12312312312") //convert String To Number Using to Two different Method
+
+
+
+//solve problem 2 -->
+
+//--> first method 
+console.log(parseFloat(str));
+
+//--> second method 
+console.log(+"12312312312"); //using unary plus
+//-->end problem 2 
